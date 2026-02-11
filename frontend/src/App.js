fb3e0d3fd68121c122cb959e09e0a0e61002b8d5
IntrustlabsIIL <ayeshasahani1303@gmail.com>
2026-02-11 17:28:01 +0530

    used ngrok https

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "https://b73d-44-192-25-247.ngrok-free.app";
+const API_URL = "https://b73d-44-192-25-247.ngrok-free.app/users";
 
 function App() {
   const [users, setUsers] = useState([]);
